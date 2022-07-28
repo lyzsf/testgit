91abb6c3262203823f0a022d1be111afab1c9fea
li <liyzsf@163.com>
2022-07-28 20:39:13 +0800

    Test

---
+++ src/mytest/mytest.java
@@ -0,0 +1,16 @@
+package mytest;
+
+import java.util.*;
+
+public class mytest {
+    public static void main(String[] args) {
+        Map<String,Integer> map_ = new HashMap<>();
+        map_.put("a",1);
+        map_.put("a",2);
+        System.out.println(map_);
+        List<Integer> a = new ArrayList<>();
+        char[] chars = "sdadadad".toCharArray();
+        Arrays.sort(chars);
+    }
+
+}
